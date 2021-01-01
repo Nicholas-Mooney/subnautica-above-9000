1a6c71ffcb04b8ef9c4b8a84b82c83e9669a4038
Nicholas-Mooney <60123281+Nicholas-Mooney@users.noreply.github.com>
2020-12-31 16:49:46 -0800

    Add files via upload

--- GUI.java
+++ GUI.java
@@ -23,30 +23,35 @@ public class GUI extends JTextPane {
 
     static int WIDTH = 1295;
     static int HEIGHT = 695;
-    Font fontLar = new Font("Monospaced", Font.BOLD, 13);
     Font fontMed = new Font("Monospaced", Font.BOLD, 14);
-    Font fontSma = new Font("Monospaced", Font.BOLD, 11);
-    public static int fontSize = 12;
-
     public GUI(){
         newTextPane(textPaneXView, fontMed, new Dimension(300, 250));
-        newTextPane(textPaneYView, fontMed, new Dimension(200, 200));
         newTextPane(textPaneXView2, fontMed, new Dimension(300, 250));
+
+        newTextPane(textPaneYView, fontMed, new Dimension(200, 200));
         newTextPane(textPaneYView2, fontMed, new Dimension(200, 200));
+
         newTextPane(textPaneHView, fontMed, new Dimension(WIDTH*3/5 - 100, HEIGHT*6/7));
         newTextPane(textPaneHView2, fontMed, new Dimension(WIDTH*3/5 - 100, HEIGHT*6/7));
-        newTextPane(errorPane, fontLar, new Dimension(WIDTH /5 - 35, HEIGHT*4/7));
+
+        newTextPane(errorPane, fontMed, new Dimension(WIDTH /5 - 35, HEIGHT*4/7));
+
         layeredPane.add(textPaneHView, 100);
-        errorPane.setText("errorPane");
         layeredPane.add(textPaneHView2, 100);
-        layeredPane.add(errorPane, 286);
+
         layeredPane.add(textPaneXView, 101);
-        layeredPane.add(textPaneYView, 101);
         layeredPane.add(textPaneXView2, 101);
+
+        layeredPane.add(textPaneYView, 101);
         layeredPane.add(textPaneYView2, 101);
+
+        layeredPane.add(errorPane, 286);
+
         layeredPane.setBackground(Color.black);
         layeredPane.setPreferredSize(new Dimension((int) WIDTH, (int) HEIGHT));
         layeredPane.setFocusable(true);
+
+        errorPane.setText("errorPane");
         Point p = new Point(10, 10);
         Point p2 = new Point(700, 0);
         Point p3 = new Point(700, 250);
@@ -59,6 +64,7 @@ public class GUI extends JTextPane {
         textPaneYView.setLocation(p3);
         textPaneXView2.setLocation(p2);
         textPaneYView2.setLocation(p3);
+
         textPaneYView.setText("hello");
         textPaneXView.setText("Hello");
         panel.add(layeredPane);
@@ -83,11 +89,9 @@ public class GUI extends JTextPane {
 
     }
     public void newTextPane(JTextAreaA infoPane, Font font, Dimension dim) {
-        infoPane.setVisible(true);
         infoPane.setPreferredSize(dim);
         infoPane.setSize(dim);
         infoPane.setText("textPane");
-        //infoPane.setBorder(BorderFactory.createLineBorder(Color.white));
         infoPane.setAlignmentX(Component.RIGHT_ALIGNMENT);
         infoPane.setFont(font);
         infoPane.setOpaque(true);
@@ -95,8 +99,6 @@ public class GUI extends JTextPane {
         infoPane.setFocusable(false);
         infoPane.setBackground(Color.black);
         infoPane.setVisible(true);
-
-        //infoPane.setBorder(BorderFactory.createLineBorder(Color.white));
     }
     public void setFrame() {
         frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
@@ -114,36 +116,11 @@ public class GUI extends JTextPane {
         comp.addKeyListener(new KeyListener() {
             @Override
             public void keyTyped(KeyEvent e) {
-
             }
-
             @Override
             public void keyPressed(KeyEvent e) {
-
                 int keyCode = e.getKeyCode();
-
                 switch (keyCode) {
-                    /*
-                    case (KeyEvent.VK_F5) -> {
-                        fontSize++;
-                        try {
-                            textPaneHView.setFont(new Font("Monospaced", Font.BOLD, fontSize));
-                            textPaneHView2.setFont(new Font("Monospaced", Font.BOLD, fontSize));
-                        } catch (Exception ignored) {
-                            fontSize--;
-                        }
-                    }
-                    case (KeyEvent.VK_F6) -> {
-                        fontSize--;
-                        try {
-                            textPaneHView.setFont(new Font("Monospaced", Font.BOLD, fontSize));
-                            textPaneHView2.setFont(new Font("Monospaced", Font.BOLD, fontSize));
-                        } catch (Exception ignored) {
-                            fontSize++;
-                        }
-                    }
-
-                     */
                     case (KeyEvent.VK_W) -> {
                         MainLoop.input = "w";
                     }
@@ -163,20 +140,15 @@ public class GUI extends JTextPane {
                     case () -> {
 
                     }
-
                      */
                 }
-
             }
-
             @Override
             public void keyReleased(KeyEvent e) {
                 int keyCode = e.getKeyCode();
                 switch (keyCode) {
-
                 }
             }
-
         });
     }
     public void errorFieldUpdater(String string, Color color) { ;
@@ -195,6 +167,4 @@ public class GUI extends JTextPane {
             errorPane.append("\n", linesColor.get(x));
         }
     }
-
-
 }
--- MainLoop.java
+++ MainLoop.java
@@ -9,8 +9,8 @@ public class MainLoop {
     public static int playerY = 50;
     public static int map_sizeX = 1000;
     public static int map_sizeY = 1000;
-    public static int viewportX = 10;
-    public static int viewportY = 5;
+    public static int viewportX = 15;
+    public static int viewportY = 10;
 
     public static int count = 0; ///THIS
 
@@ -26,7 +26,8 @@ public class MainLoop {
             wait(100);
             display(map);
             processInput();
-        }
+            input = "";
+      }
     }
     public static void wait(int x) {
         try {
@@ -53,20 +54,17 @@ public class MainLoop {
 
         for (int y = py - viewportY; y <=  py + viewportY; y++) {
             for (int x = px - viewportX; x < px + viewportX; x++) {
-
                 if (y == playerY && x == playerX) {
                     textPane.append("P ");
                 } else {
                     if(x >= mapGrid.maxX || y >= mapGrid.maxY || y < 0 || x < 0){
-                        textPane.append("| ");
-                        System.out.print("| ");
+                        textPane.append("| ", Color.white);
+                        //System.out.print("| ");
                     }else {
-                        System.out.print(displayForTile(mapGrid.map[x][y]));
-                        textPane.append(displayForTile(mapGrid.map[x][y]) + " ");
+                        //System.out.print(displayForTile(mapGrid.map[x][y]));
+                        textPane.append(charForTile(mapGrid.map[x][y]) + " ", colorForTile(mapGrid.map[x][y]));
                     }
                 }
-
-
             }
             textPane.append("\n", Color.white);
         }
@@ -77,9 +75,6 @@ public class MainLoop {
 
 
     //input functions
-    public static void input() {
-        input = sc.nextLine();
-    }
     public static void processInput() {
         if (input.equals("a")) {
             playerX--;
@@ -117,20 +112,20 @@ public class MainLoop {
         for (int y = playerY - viewportY; y < playerY + viewportY; y++) {
             for (int x = playerX - viewportX; x < playerX + viewportX; x++) {
                 if (y == playerY && x == playerX) {
-                    System.out.print("P ");
+                    //System.out.print("P ");
                 } else {
                     if(x >= mapGrid.maxX || y >= mapGrid.maxY || y < 0 || x < 0){
-                        System.out.print("| ");
+                        //System.out.print("| ");
                     }else {
-                        System.out.print(displayForTile(mapGrid.map[x][y]));
-                        System.out.print(" ");
+                        //System.out.print(displayForTile(mapGrid.map[x][y]));
+                        //System.out.print(" ");
                     }
                 }
             }
-            System.out.println(" ");
+            //System.out.println(" ");
         }
     }
-    public static char displayForTile(tileSet tile){
+    public static char charForTile(tileSet tile){
         switch(tile.tileType){
             case "air":
                 return '\'';
@@ -142,6 +137,20 @@ public class MainLoop {
                 return 'o';
             default:
                 return '!';
+        }
+    }
+    public static Color colorForTile(tileSet tile){
+        switch(tile.tileType){
+            case "air":
+                return Color.white;
+            case "water":
+                return Color.BLUE;
+            case "earth":
+                return new Color(87, 71, 46);
+            case "ore":
+                return Color.GRAY;
+            default:
+                return Color.RED;
         }
     }
 }
