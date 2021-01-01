89d23bb8a4f6654b7d03673015a5f8ea3a16b11c
Nicholas-Mooney <60123281+Nicholas-Mooney@users.noreply.github.com>
2020-12-31 22:18:33 -0800

    Add files via upload

---
+++ playerInv.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class playerInv {
+public int numItems = 0;
+public ArrayList<String> items = new ArrayList<String>();
+
+public void addItem(String item){
+    numItems++;
+    items.add(item);
+}
+public void deleteItem(String item){
+    numItems--;
+    items.remove(item);
+}
+public boolean hasItem(String item){
+    return (items.contains(item));
+}
+public void resetItems(){
+    while(numItems > 0){
+        items.remove(0);
+        numItems--;
+    }
+}
+public void die(){
+    Random rand = new Random();
+    for(int i = numItems/2; i > 0; i--){
+        items.remove(rand.nextInt(numItems));
+    }
+}
+
+}
