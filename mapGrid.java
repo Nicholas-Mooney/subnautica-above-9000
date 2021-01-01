e473f5845c05e57d3e72efa10c6f16a34ad6d1c8
pmooney123 <67892202+pmooney123@users.noreply.github.com>
2020-12-31 21:29:10 -0800

    Add files via upload

--- mapGrid.java
+++ mapGrid.java
@@ -1,12 +1,40 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class mapGrid {
     public static int maxX = 200;
-    public static int maxY = 50;
+    public static int maxY = 500;
     public static tileSet[][] map = new tileSet[maxX][maxY];
+    public static double[] heightmap = new double[maxX];
+    public static Random rand = new Random();
 
     mapGrid(int seed){
         String tileType;
-        for(int y = 0; y < 50; y++){
-            for(int x = 0; x < 200; x++){
+
+        //generates heightmap
+        for (int x = 0; x < maxX; x++) {
+
+            double slope = 0.25; //slope of the terrain before modifications (slope downwards)
+            double sin_weight = 10; //heigher values will amke the terrain look like a sin graph
+            double noise_weight = 1; ///heigher values makes the terrain more noisier and random
+
+            //adds random noise
+            heightmap[x] =  noise_weight * SimplexNoise.noise(rand.nextInt(10),0);
+
+            //slope trend of terrain
+            heightmap[x] = heightmap[x] - 10 + slope * x;
+
+            //adds sinosoidal noise
+            heightmap[x] = heightmap[x] + sin_weight * Math.sin(0.25 * x);
+
+        }
+
+
+
+
+        //labels tiles air/water based on integer
+        for(int y = 0; y < maxY; y++){
+            for(int x = 0; x < maxX; x++){
                 if(y<10){
                     tileType = "air";
                 }else{
@@ -16,5 +44,37 @@ public class mapGrid {
                 map[x][y] = a;
             }
         }
+
+        //labels tiles water/earth based on height map
+        for(int y = 0; y < maxY; y++){
+            for(int x = 0; x < maxX; x++){
+                if(y<20 + heightmap[x]){
+                    tileType = "water";
+                }else{
+                    tileType = "earth";
+                }
+                tileSet a = new tileSet(tileType);
+                map[x][y] = a;
+            }
+        }
+
+        //labels tiles ore
+
+        int OreFrequency = 50;//(heigher = rarer)
+
+        for(int y = 0; y < maxY; y++){
+            for(int x = 0; x < maxX; x++){
+                if(map[x][y].tileType.equals("earth")) {
+                    if (rand.nextInt(OreFrequency) == 0) {
+                        map[x][y].tileType = "ore";
+                    }
+                }
+            }
+        }
+
     }
+
+
+
+
 }
